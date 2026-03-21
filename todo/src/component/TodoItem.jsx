ddf0b2fb54672a3ce167264fe1b7b90ee55aeea2
Rani <saini12.rani@gmail.com>
2026-03-21 18:25:05 +0530

    I have added the delete functionality

--- todo/src/component/TodoItem.jsx
+++ todo/src/component/TodoItem.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdDelete } from "react-icons/md";
+import { MdOutlineDone } from "react-icons/md";
+function TodoItem({item,deleteItem,mark}) {
+  console.log(item,deleteItem,mark)
+return (
+    <div>
+      {item.map((items, index) => {
+            return (
+             <ul key={index}>
+              <li >{items} <button onClick={()=>deleteItem(index)}> <MdDelete/></button> <span>{ mark ? <MdOutlineDone />: ""}</span> </li>
+             </ul>
+            );
+          })}
 
-function TodoItem() {
-  return (
-          <div>
-        <li></li>
-      </div>
-
+    </div>
+   
   )
 }
-
-export default TodoItem
+export default TodoItem;
--- todo/src/component/Todolist.jsx
+++ todo/src/component/Todolist.jsx
@@ -1,34 +1,35 @@
 import React, { useState } from 'react'
-
-
+import TodoItem from './TodoItem';
+  
 function Todolist() {
-  const [input, setInput] = useState('');
-  const [item,setItem] = useState([]);
-  console.log(input)
+const [input, setInput] = useState('');
+const [item,setItem] = useState([]);
+const [mark , setMark] =  useState(false);
+
+
 function addList(){
-    //console.log(e.target.value);
-    //const getValue = input;
-   // console.log(getValue)
-    setItem([item, input]);
+   if(input== 0) return;
+    setItem([...item, input]);
+    setMark(true)
     setInput("")
 }
+
+function deleteItem(indexDelete){
+//console.log(index)
+const StoreDelete =  item.filter((_,index)=> index !== indexDelete);
+setItem(StoreDelete);
+}
+
 return (
     <div>
-    <div className='todoitem'>
+      <div className='todoitem'>
       <input type="text" value={input} placeholder='Enter the task' onChange={(e)=>setInput(e.target.value)}/>
       <button onClick={()=>addList()} >Add</button>
 
     </div>
-        <h1>{console.log(item)}
-          {item.map((items, index) => {
-            return (
-              <h1 key={index}>{items}</h1>
-            );
-          })}
-    </h1>
+      <TodoItem   deleteItem={deleteItem} mark={mark} item={item}/>
     </div>
-   
   )
 }
 
-export default Todolist;
+export default Todolist
